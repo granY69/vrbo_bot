78783bd84fbddc786b16c3dd2ad8e4224f43303c
levI69 <cbymin@gmail.com>
2023-03-25 08:43:56 +0500

    data_validated_fixed

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def run(playwright: Playwright, url : str) -> None:
     page.evaluate('window.scrollTo(0, 5000);')
     for _ in range(12):
         calender = page.locator('div.calendar')
-        calenders.append(calender.inner_html())
+        calender_html = calender.inner_html()
+        calenders.append(calender_html)
         page.get_by_role("button", name="Next month").click()
         time.sleep(1)
 
@@ -28,27 +29,28 @@ if __name__ == "__main__":
     url = "https://www.vrbo.com/1753814"
     with sync_playwright() as playwright:
         calenders = run(playwright, url)
-
     values = []
     for calender in calenders:
         etree = html.fromstring(calender)
         months = etree.xpath("//div[@class='month multi simple']")
         for month in months:
-            date_month = month.xpath("h4/span")[0].text
-            items = month.xpath("table/tbody/tr/td/div/div/div[2]")
-            for item in items:
-                day = item.xpath("div[1]")[0].text
-                try:
-                    price = item.xpath("div[2]")[0].text
-                except:
-                    price = None
-                date = day + " " + date_month
-                date = datetime(2025, 2, 28, 0, 0)
-                values.append((date, price))
+            tds = month.xpath("table/tbody/tr/td[contains(@aria-label,'') and @aria-hidden='false']")
+            for td in tds:
+                date_text = td.xpath("@aria-label")[0]
+                is_available = "inavailable"
+                price = None
+                if not " is unavailable." in date_text:
+                    try:
+                        price = td.xpath("div/div/div[2]/div[2]")[0].text
+                        is_available = "available"
+                    except:
+                        pass
+                date_obj = datetime.strptime(date_text.replace(" is unavailable.", ""), "%B %d, %Y")
+                values.append((url, date_obj, is_available, price))
     
-    connection = DatabaseConnection(os.environ.get("DB_USERNAME"), os.environ.get("DB_PASSWORD"), os.environ.get("DB_NAME"))
+    connection = DatabaseConnection(os.environ.get("DB_USERNAME"), os.environ.get("DB_PASSWORD"), os.environ.get("DB_NAME"), os.environ.get("DB_HOST"))
     connection.connection()
-    query = """ insert into availability_date_price(date, price) values(%s, %s); """
+    query = """ insert into availability_date_price(url, date, is_available, price) values(%s, %s, %s, %s); """
     connection.cursor.executemany(query, values)
     connection.db.commit()
     print("Data Inserted in Db")
